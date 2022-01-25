9dae74688ab685e4eab433109ca736e71132ed2e
camas002 <camas002@fiu.edu>
2022-01-25 17:29:55 -0500

    Adding function to check ethereum wallet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Kryptomuz from '../abis/Kryptomuz.json'
 
 class App extends Component {
+
+    async componentDidMount(){
+        await this.loadWeb3;
+    }
+
+    // detect ethereum provider
+    async loadWeb3() {
+
+        const provider = await detectEthereumProvider();
+
+        // if there is a provider, log that it is working
+        if(provider) {
+            console.log('ethereum wallet is connected')
+            window.web3 = new Web3(provider)
+        } else {
+            console.log('no ethereum wallet detected')
+        }
+    }
 
     render() {
         return (
