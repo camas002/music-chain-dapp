51bba3ff7cd807ce51dd7676a42a3aca1e839e68
Manuel Kamboykos <60014618+Mkamboykos@users.noreply.github.com>
2022-01-25 23:45:54 -0500

    added comments

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,24 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.4.22 <0.9.0;  //truffle config version "^0.8.0"
 
 contract Migrations {
   address public owner = msg.sender;
   uint public last_completed_migration;
 
-  modifier restricted() {
+  modifier restricted() {  //restriction takes in two values
     require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
+      msg.sender == owner,  //smart contract restriction for the sender to be the owner
+      "This function is restricted to the contract's owner!"  // if restriction is false
     );
-    _;
+    _;  //continue running the function
   }
 
+  //sending migration out 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 
+
+  //update smart contract migrations
   function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+    Migrations upgraded = Migrations(new_address);  //grab the Migrations contract from line 4 and make it a new Migrations route
+    upgraded.setCompleted(last_completed_migration); //set the new migration by using setCompleted from line 17 and set the last completed migration from line 6
   }
 }
